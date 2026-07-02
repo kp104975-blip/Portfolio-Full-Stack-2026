1867c2c1b50ef740328e3c1fda5b08cac5c56678
kp104975-blip <kp104975@gmail.com>
2026-07-02 18:32:28 +0530

    rgre

--- Server/config/connectDB.js
+++ Server/config/connectDB.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.Live_URL);
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(process.env.Live_URL);
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Failed");
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
